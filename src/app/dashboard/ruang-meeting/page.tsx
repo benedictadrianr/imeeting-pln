7f4f023816fd2103926be04670098e7f1f9f5be8
Benedict Adrian <benedictadrianr@gmail.com>
2025-05-12 16:25:16 +0700

    Add dynamic export to getReservedRoom for server-side rendering

--- src/app/dashboard/ruang-meeting/page.tsx
+++ src/app/dashboard/ruang-meeting/page.tsx
@@ -1,11 +1,16 @@
-"use server";
+export const dynamic = "force-dynamic";
 
 import RoomReservationList from "@/components/pesan-ruang-meeting/room-reservation-list";
-import { getReservedRoom } from "@/data/getReservedRoom";
+// import { getReservedRoom } from "@/data/getReservedRoom";
+import { db } from "@/drizzle/db";
+import { ReservationTable, selectReservationSchema } from "@/drizzle/schema";
 import React from "react";
+import { z } from "zod";
 
 const RuangMeeting = async () => {
-  const result = await getReservedRoom();
+  const result = (await db.select().from(ReservationTable)) as z.infer<
+    typeof selectReservationSchema
+  >[];
   console.log("Reserved room:", result);
   return (
     <div>
