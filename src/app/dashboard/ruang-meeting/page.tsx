1c7d04cce171a4f89e01e43eac5a991f4b048449
Benedict Adrian <benedictadrianr@gmail.com>
2025-05-13 15:11:03 +0700

    Sort room reservations by creation date in RuangMeeting component

--- src/app/dashboard/ruang-meeting/page.tsx
+++ src/app/dashboard/ruang-meeting/page.tsx
@@ -3,11 +3,15 @@ export const dynamic = "force-dynamic";
 import RoomReservationList from "@/components/pesan-ruang-meeting/room-reservation-list";
 import { db } from "@/drizzle/db";
 import { ReservationTable, selectReservationSchema } from "@/drizzle/schema";
+import { desc } from "drizzle-orm";
 import React from "react";
 import { z } from "zod";
 
 const RuangMeeting = async () => {
-  const result = (await db.select().from(ReservationTable)) as z.infer<
+  const result = (await db
+    .select()
+    .from(ReservationTable)
+    .orderBy(desc(ReservationTable.createdAt))) as z.infer<
     typeof selectReservationSchema
   >[];
   return (
